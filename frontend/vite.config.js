372431a51d49ea571079eae20628d81c7ee53263
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2025-02-06 08:06:05 +0530

    forgot to build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://creditsea-fullstack-engineer-assignment.onrender.com/',
+      '/api': 'https://creditsea-fullstack-engineer-assignment.onrender.com',
     },
   },
   plugins: [react()],
